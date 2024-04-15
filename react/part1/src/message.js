c6128b8090ed060270eea7c4ba931e18a574dae2
Juan Jorquera <jjjuanhugo@gmail.com>
2024-04-15 01:19:57 -0400

    subiendo ejercicios aun no resueltos de react

--- react/part1/src/message.js
+++ react/part1/src/message.js
@@ -1,5 +1,7 @@
 const Mensaje = (props) => {
-    return <h1 style={{color: props.color}}  >{props.message}</h1> // asi es como tenemos un atributo con desde otro archivo js
+    return <h1 
+    style={{color: props.color}}  >
+        {props.message}</h1> // asi es como tenemos un atributo con desde otro archivo js
 }
 
 //* ASi exportamos el codigo para reutilizarlo en otro codigo archivo JS
